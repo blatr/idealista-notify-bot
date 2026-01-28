8a93ed5038a01cbd1e2684417e9db7513693d185
Максим Остроухов <max@MacBookPro.home>
2026-01-28 02:25:59 +0100

    add bot logic

--- webapp/services/__init__.py
+++ webapp/services/__init__.py
@@ -1,4 +1,1 @@
-from webapp.services.listing_service import ListingService
-from webapp.services.scraper_service import ScraperService
-
-__all__ = ["ListingService", "ScraperService"]
+__all__ = []
